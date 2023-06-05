dd13f8cefd9eb722b6b0e4e1b7fec9a99ff32ccb
Jacob Hummer <jcbhmr@outlook.com>
2023-06-04 21:15:39 -0500

    Completely remake

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * @see {@link https://github.com/okonet/lint-staged#configuration}
- */
-module.exports = {
-  '**/*.ts': ['tsc', 'prettier --write', 'eslint --fix'],
-  '**/*.js': ['prettier --write', 'eslint --fix'],
-  '**/*.json': ['prettier --write'],
-};
--- prepare-hooks.js
+++
@@ -1,6 +0,0 @@
-const isLocalEnv = !process.env.CI;
-
-if (isLocalEnv) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('husky').install();
-}
